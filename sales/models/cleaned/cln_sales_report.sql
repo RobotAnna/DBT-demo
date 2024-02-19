7074365536d9f7db912e0c403da3cf68229636cf
Nerrida D <nerd@robotanna.com>
2024-02-19 08:27:21 +0100

    add boiler-plate template code to model

--- sales/models/cleaned/cln_sales_report.sql
+++ sales/models/cleaned/cln_sales_report.sql
@@ -1,34 +1,47 @@
+-- IMPORT
 with source as (
       select * from {{ source('staged', 'amazon_sale_report') }}
 ),
-renamed as (
+select * from {{ ref('Indian_States') }}
+;
+
+/*
+-- CLEANING
+
+cleaned as (
     select
-        {{ adapter.quote("index") }},
-        {{ adapter.quote("Order ID") }},
-        {{ adapter.quote("Date") }},
-        {{ adapter.quote("Status") }},
-        {{ adapter.quote("Fulfilment") }},
-        {{ adapter.quote("Sales Channel") }},
-        {{ adapter.quote("ship-service-level") }},
-        {{ adapter.quote("Style") }},
-        {{ adapter.quote("SKU") }},
-        {{ adapter.quote("Category") }},
-        {{ adapter.quote("Size") }},
-        {{ adapter.quote("ASIN") }},
-        {{ adapter.quote("Courier Status") }},
-        {{ adapter.quote("Qty") }},
-        {{ adapter.quote("currency") }},
-        {{ adapter.quote("Amount") }},
-        {{ adapter.quote("ship-city") }},
-        {{ adapter.quote("ship-state") }},
-        {{ adapter.quote("ship-postal-code") }},
-        {{ adapter.quote("ship-country") }},
-        {{ adapter.quote("promotion-ids") }},
-        {{ adapter.quote("B2B") }},
-        {{ adapter.quote("fulfilled-by") }},
-        {{ adapter.quote("Unnamed: 22") }}
+        {{ adapter.quote("index") }} AS ORDER_INDEX,
+        {{ adapter.quote("Order ID") }} AS ORDER_ID,
+        {{ adapter.quote("Date") }} AS ORDER_DATE,
+        SPLIT_PART({{ adapter.quote("Status") }}, ' - ',1) AS ORDER_STATUS,
+        SPLIT_PART({{ adapter.quote("Status") }}, ' - ',2) AS ORDER_SUB_STATUS,
+        {{ adapter.quote("Fulfilment") }} AS FULFILMENT,
+        {{ adapter.quote("Sales Channel") }} AS SALES_CHANNEL,
+        {{ adapter.quote("ship-service-level") }} AS SHIP_SERVICE_LEVEL,
+        {{ adapter.quote("Style") }} AS PRODUCT_STYLE,
+        {{ adapter.quote("SKU") }} AS SKU,
+        {{ adapter.quote("Category") }} AS PRODUCT_CATEGORY,
+        {{ adapter.quote("Size") }} AS PRODUCT_SIZE,
+        {{ adapter.quote("ASIN") }} AS ASIN,
+        {{ adapter.quote("Courier Status") }} AS COURIER_STATUS,
+        {{ adapter.quote("Qty") }} AS QUANTITY,
+        {{ adapter.quote("currency") }} AS CURRENCY,
+        {{ adapter.quote("Amount") }} AS AMOUNT,
+        {{ adapter.quote("ship-city") }} AS SHIP_CITY,
+        {{ adapter.quote("ship-state") }} AS SHIP_STATE,
+        {{ adapter.quote("ship-postal-code") }} AS SHIP_POSTAL_CODE,
+        {{ adapter.quote("ship-country") }} AS SHIP_COUNTRY,
+        {{ adapter.quote("promotion-ids") }} AS PROMOTION_IDS,
+        {{ adapter.quote("B2B") }} AS B2B,
+        {{ adapter.quote("fulfilled-by") }} AS FULFILLED_BY
 
     from source
 )
+
+-- CUSTOM LOGIC
+
+-- FINAL CTE
+
+
 select * from renamed
-  
+*/
